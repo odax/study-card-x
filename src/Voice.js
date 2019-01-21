95347626c00d62588ff1062a418cdf0bdb46309d
odax <nr10@albion.edu>
2019-01-21 01:34:20 -0500

    added voice.js

---
+++ src/Voice.js
@@ -0,0 +1,35 @@
+export const greeting = {
+    greeting: 'Welcome to Study Card X, have you been here before?',
+    no: 'Would you like to create an account or practice as a guest?',
+    guest: 'Okay, would you like a quick tutorial?',
+    yes: 'Sweet, tell me a little about yourself..'
+}
+
+export const newUser = {
+    new1a9: 'This will only take 1 minute',
+    new2a9: 'Study Card X was designed to accerlate the time it takes for you to learn',
+    new3a9: 'First you create two study cards',
+    new4a9: 'Then I will help you start studying',
+    new5a9: 'Depending on diffulty, you will eventually master this card!',
+    new6a9: 'This when you get 5, 10, or 20 in a row correct depending on its difficulty',
+    new7a9: 'When you master a card, Ill have you add a new card!',
+    new8a9: 'We will then repeat the cycle! Do you have any questions?',
+    new9a9: 'Awesome, lets get started'
+
+}
+
+export const oldUser = {
+    old1a1: 'Great, remind again who you are?'
+}
+
+export const guestStudy = {
+    addQuestion: 'Add a question',
+    addAnswer: 'Okay, now add an answer',
+    difficulty: 'How hard is this question?',
+    asking: 'Whats the answer?',
+    success1a2: 'Nice ',
+    success2a2: 'more in a row to master this one',
+    failure: 'Its okay we can try it again',
+    master: 'Great job! You mastered that question. Click that new button if you want to archive it.'
+}
+
