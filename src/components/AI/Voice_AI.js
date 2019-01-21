b671dbbd7d12f61b9304fad536d29630b0538e1b
odax <nr10@albion.edu>
2019-01-21 02:32:34 -0500

    centered app inside of body

--- src/components/AI/Voice_AI.js
+++ src/components/AI/Voice_AI.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import TextyAnim from "rc-texty";
+import './Voice_AI.css';
 
 export default class Voice_AI extends Component {
   render() {
     return (
-      <div className=''>
+      <div className='Voice_AI__Text'>
         <TextyAnim>{this.props.contextState.voiceState.greeting}</TextyAnim>
       </div>
     );
