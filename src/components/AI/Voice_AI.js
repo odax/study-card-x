f02395826b004710f9e53d5f446be3dc72ee7bb8
odax <nr10@albion.edu>
2019-01-21 02:17:20 -0500

    added animation, edited readme

--- src/components/AI/Voice_AI.js
+++ src/components/AI/Voice_AI.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import TextyAnim from "rc-texty";
 
 export default class Voice_AI extends Component {
   render() {
     return (
-      <div>
-        {this.props.contextState.voiceState.greeting}
+      <div className=''>
+        <TextyAnim>{this.props.contextState.voiceState.greeting}</TextyAnim>
       </div>
-    )
+    );
   }
 }
