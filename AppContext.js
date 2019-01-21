018be8afb638db2f5eba01134d6e6e49dc4185ea
odax <nr10@albion.edu>
2019-01-21 00:55:08 -0500

    import context provider into index.js

--- AppContext.js
+++ AppContext.js
@@ -1,34 +1,35 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 //create new context
 const AppContext = React.createContext();
 
 export class AppContextProvider extends Component {
-    state = {
-        thisIsState: true
-    }
+  state = {
+    thisIsState: true
+  };
   //methods here
   thisIsStateChecker = () => {
-      if (this.state.thisIsState === true) {
-          console.log('We have state!')
-      }
-  }
+    if (this.state.thisIsState === true) {
+      console.log("We have state!");
+    }
+  };
   render() {
     return (
       <AppContext.Provider
         value={{
-            AppState: this.state,
-            checkHaveState: this.thisIsStateChecker
-        }}>
+          AppState: this.state,
+          checkHaveState: this.thisIsStateChecker
+        }}
+      >
         {this.props.children}
-        </AppContext.Provider>
+      </AppContext.Provider>
     );
   }
 }
 
 AppContextProvider.propTypes = {
-    children: PropTypes.any
+  children: PropTypes.any
 };
 
 export const AppContextConsumer = AppContext.Consumer;
