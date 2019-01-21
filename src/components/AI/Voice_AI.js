896553b1a63167b4850b8d1593032b3ea2fa98e9
odax <nr10@albion.edu>
2019-01-21 16:26:27 -0500

    removed react-pose

--- src/components/AI/Voice_AI.js
+++ src/components/AI/Voice_AI.js
@@ -1,30 +1,13 @@
 import React, { Component } from "react";
-import posed from "react-pose";
 import "./Voice_AI.css";
 
-const Box = posed.div({
-    visible: { opacity: 1 },
-    hidden: { opacity: 0 }
-  });
-
 export default class Voice_AI extends Component {
-    state = {
-        isVisible: true
-    }
-  phrase = () => {
-    console.log("finished!");
-  };
+
   render() {
     return (
       <div>
         {this.props.contextState.voiceState[0].text}
-        <Box
-    className="box"
-    pose={this.state.isVisible ? 'visible' : 'hidden'}
-  />
       </div>
     );
   }
 }
-
-//just uninstalled text thing, added pose, need to create pose animation now
