dcc2f6ceb09f9ee771876dffdbda11dc4788e435
odax <nr10@albion.edu>
2019-02-07 17:54:58 -0500

    added flip animation

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,18 +4,23 @@ import "./Card.css";
 export default class Card extends Component {
   render() {
     return (
-      <div className="Card__Card-Container container">
-        <div className="Card-Container__Card card green lighten-2">
-          <div className="Card__Card-Content">
-            <span className="Card-Content__Card-Title">Question</span>
-            <div className="Card-Content__Card-Question">
-              <form>
-                <input type="textarea" />
-              </form>
+      <div className="Card__Flip-Container container">
+        <div className="Flip-Container__Flipper">
+          <div className="Flipper__Front">
+            <div className="Front__Card card green lighten-2">
+              <div className="Card__Card-Content">
+                <span className="Card-Content__Card-Title">Question</span>
+                <div className="Card-Content__Card-Question">
+                  <form>
+                    <input type="textarea" />
+                  </form>
+                </div>
+              </div>
             </div>
           </div>
+          <div className="Flipper__Back">back</div>
         </div>
       </div>
     );
   }
-};
+}
